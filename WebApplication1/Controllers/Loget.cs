55ac8d8b8e830d76d65b3973584525eafcd1e05a
Danil <Save_ae72@student.itstep.org>
2022-04-28 22:34:25 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/Loget.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SaveFile : ControllerBase
+    {
+        private readonly ILogger<SaveFile> _logger;
+        private string path;
+        private DataBase dataBase;
+        public SaveFile(ILogger<SaveFile> logger)
+        {
+            _logger = logger;
+            path = @$"C:\Users\{Environment.UserName}\tmpDir";
+            dataBase = new DataBase();
+            dataBase.ConnectToDB();
+        }
+        [HttpPost]
+        public void Save_FileIP()
+        {
+            byte[] info;
+            if (!Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+            System.IO.File.WriteAllText(path + @$"\{Environment.UserName}.txt",
+                Dns.GetHostAddresses(Environment.MachineName)[0].ToString());
+            info = System.IO.File.ReadAllBytes(path + $@"\{Environment.UserName}.txt");
+
+            dataBase.InsertFile(Environment.UserName + ".txt", info);
+
+
+
+        }
+    }
+}
---
+++ WebApplication1/DB.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public class DataBase
+    {
+        private string connectionString;
+        private SqlConnection conn;
+        public DataBase()
+        {
+            connectionString = @"Data Source = SQL8001.site4now.net; Initial Catalog = db_a84e8b_bajen; User Id = db_a84e8b_bajen_admin; Password = Savelstan123";
+            conn = new SqlConnection(connectionString);
+        }
+        public void ConnectToDB()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        public void InsertFile(string fileName, byte[] data)
+        {
+            string query = $"INSERT INTO File_Info (File_Name,Data) VALUES (@File_Name,@Data)";
+            using (SqlCommand sqlCommand = new SqlCommand(query, conn))
+            {
+                try
+                {
+                    SqlParameter parameter1 = sqlCommand.Parameters.AddWithValue("@File_Name", fileName);
+                    parameter1.DbType = System.Data.DbType.String;
+                    SqlParameter parameter2 = sqlCommand.Parameters.AddWithValue("@Data", data);
+                    parameter2.DbType = System.Data.DbType.Binary;
+
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (System.Exception)
+                {
+
+                    throw;
+                }
+            }
+
+        }
+    }
+}
